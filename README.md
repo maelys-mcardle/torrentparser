1bf60377d5016f88ea5e349cdcfb350cb670c26f
Maëlys <airseal@hotmail.com>
2016-12-12 15:02:17 -0500

    Added setup.py, sample torrent files, directory structure.

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# torrentparser
-A native Python implementation to parse Bittorrent files.
+# BitTorrent Parser
+A native Python implementation of a BitTorrent file parser.
+
+# Author
+Maelys McArdle ([maelys.bio](http://maelys.bio))
+
+# License
+MIT
+
+# Installation
+
+Installation instructions were tested on `Fedora 25` against `Python 2.7.12`.
+
+### Installation
+
+To install the `torrentparser` package, run the following command:
+
+    pip install .
+    
+Alternatively, you can run the following command:
+
+    python -m pip install .
+    
+### Installation (Development Mode)
+
+To install in editable mode for developers, run the following command:
+    
+    pip install -e .
+    
+### Uninstallation
+To uninstall the package, run the following command:
+
+    pip uninstall torrentparser
+
+# Running Tests
+
+To run tests, simply do:
+
+    python setup.py test
---
+++ setup.py
@@ -0,0 +1,56 @@
+"""The setup file for the BitTorrent parser.
+"""
+
+from setuptools import setup
+
+setup(
+
+    # General information.
+    name='torrentparser',
+    version='0.0.1',
+    description='A parser for BitTorrent files.',
+    url='https://github.com/maelys-mcardle/torrentparser',
+    license='MIT',
+    keywords='BitTorrent',
+
+    # Author information.
+    author='Maelys McArdle',
+    author_email='spam@maelys.bio',
+
+    # Classifiers (compatible version of Python, etc.)
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # The supported version of Python.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # Parser package.
+    packages=['torrentparser'],
+
+    # Run-time dependencies.
+    install_requires=[],
+
+    # Additional dependencies (for tests.)
+    extras_require={
+        'tests': ['coverage'],
+    },
+
+    # Entry points.
+    entry_points={
+        'console_scripts': [
+            'torrentparser=torrentparser:main',
+        ],
+    },
+)
