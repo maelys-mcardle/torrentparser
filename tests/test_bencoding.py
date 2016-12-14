6795fcf549eecf153721777e3cd0fb082fc708ee
Maëlys <airseal@hotmail.com>
2016-12-13 20:06:37 -0500

    Added bencoding decoder for integers and simple tests.

--- tests/test_bencoding.py
+++ tests/test_bencoding.py
@@ -1,5 +1,5 @@
 import unittest
-from torrentparser.bencoding import decode_byte_string
+from torrentparser.bencoding import decode_byte_string, decode_integer, BencodingDecodeException
 
 
 class TestBencodingByteString(unittest.TestCase):
@@ -12,4 +12,23 @@ class TestBencodingByteString(unittest.TestCase):
 
     def test_zero_length(self):
         self.assertEqual(decode_byte_string("0:"), ('', 2))
+
+
+class TestBencodingInteger(unittest.TestCase):
+
+    def test_empty(self):
+        with self.assertRaises(BencodingDecodeException):
+            decode_integer("ie")
+
+    def test_zero(self):
+        self.assertEqual(decode_integer("i0e"), (0, 3))
+
+    def test_positive(self):
+        self.assertEqual(decode_integer("i365e"), (365, 5))
+
+    def test_negative(self):
+        self.assertEqual(decode_integer("i-65e"), (-65, 5))
+
+    def test_64bit(self):
+        self.assertEqual(decode_integer("i18446744073709551615e"), (18446744073709551615, 22))
 
--- torrentparser/bencoding.py
+++ torrentparser/bencoding.py
@@ -1,12 +1,13 @@
+"""
+Bencoding decoding functions.
+"""
+
 
 def decode_byte_string(encoded_byte_string):
-    """Decodes Bencoding byte strings.
-
-    Byte strings have the format:
-        <string length encoded in base ten ASCII>:<string data>
-
-    Example:
-        5:hello
+    """
+    Decodes a Bencoding byte string.
+    :param encoded_byte_string: The byte string in Bencoding.
+    :return: Tuple containing the byte string and the end byte index.
     """
 
     # Find the colon.
@@ -45,24 +46,145 @@ def decode_byte_string(encoded_byte_string):
                 return decoded_byte_string, byte_string_end
 
 
-def decode_dictionary(dictionary):
-    """Decodes Bencoding dictionaries.
-
-    Dictionaries have the format:
-        d<string length encoded in base ten ASCII>:<string data>e
-
-    Example:
-        d3:cow3:moo4:spam4:eggse
+def decode_integer(encoded_integer):
     """
-    # First byte needs to be a "d" to signify that it's a Bencoding dictionary.
-    if dictionary[0] != "d":
-        raise BencodingDecodeException("Not a dictionary.")
+    Decodes a Bencoding integer.
+    :param encoded_integer: The integer in Bencoding.
+    :return: Tuple containing the integer and the end byte index.
+    """
 
-    return dict()
+    # First byte needs to be a "i" to signify that it's a Bencoding integer.
+    if len(encoded_integer) == 0 or encoded_integer[0] != "i":
+        raise BencodingDecodeException("Not an integer")
+
+    # Find the end.
+    end_position = encoded_integer.find("e")
+
+    # There wasn't an end.
+    if end_position == -1:
+        raise BencodingDecodeException("Invalid integer (missing terminating 'e')")
+
+    # Get the integer.
+    integer_as_string = encoded_integer[1:end_position]
+
+    # If the string is empty, indicate an error.
+    if len(integer_as_string) == 0:
+        raise BencodingDecodeException("Invalid integer (missing value)")
+
+    # Try parsing the integer. If it's valid, return it.
+    try:
+        integer = int(integer_as_string)
+        return integer, end_position + 1
+
+    # Value is invalid, indicate error.
+    except ValueError:
+        raise BencodingDecodeException("Invalid integer (not a number)")
+
+
+def decode_list(encoded_list):
+    """
+    Decodes a Bencoding list.
+    :param encoded_list:
+    :return:
+    """
+
+    # First byte needs to be a "l" to signify that it's a Bencoding list.
+    if len(encoded_list) == 0 or encoded_list[0] != "l":
+        raise BencodingDecodeException("Not a list")
+
+    # Initialize an empty list.
+    decoded_list = list()
+    start_position = 1
+
+    # Continue reading.
+    while True:
+
+        # Start position unreadable.
+        if start_position >= len(encoded_list):
+            raise BencodingDecodeException("Invalid list (missing data)")
+
+        #
+        first_byte = encoded_list[start_position]
+
+        # List is finished.
+        if first_byte == "e":
+            break
+
+        # List item: integer.
+        elif first_byte == "i":
+
+        # List item: nested list.
+        elif first_byte == "l":
+
+        # List item: dictionary.
+        elif first_byte == "d":
+
+        # List item: byte string.
+        elif first_byte.isdigit():
+
+        # Unknown list item.
+        else:
+
+
+def is_byte_string(encoded_byte_string):
+    """
+    Convenience function to identify whether it's a byte string.
+    :param encoded_byte_string: The byte string in Bencoding.
+    :return: Boolean indicating if this is a byte string.
+    """
+
+    try:
+        decode_byte_string(encoded_byte_string)
+        return True
+    except BencodingDecodeException:
+        return False
+
+
+def is_integer(encoded_integer):
+    """
+    Convenience function to identify whether it's an integer.
+    :param encoded_integer: The integer in Bencoding.
+    :return: Boolean indicating if this is an integer.
+    """
+
+    try:
+        decode_integer(encoded_integer)
+        return True
+    except BencodingDecodeException:
+        return False
+
+
+def decode_dictionary(encoded_dictionary):
+    """
+    Decodes Bencoding dictionaries.
+    """
+
+    # First byte needs to be a "d" to signify that it's a Bencoding dictionary.
+    if len(encoded_dictionary) == 0 or encoded_dictionary[0] != "d":
+        raise BencodingDecodeException("Not a dictionary")
+
+    start_position = 1
+    decoded_dictionary = dict()
+
+    # Continue reading.
+    while True:
+
+        # Start position unreadable.
+        if start_position >= len(encoded_dictionary):
+            raise BencodingDecodeException("Invalid dictionary (missing data)")
+
+        # Dictionary is finished.
+        if encoded_dictionary[start_position] == "e":
+            break
+
+        # Read
+
+    return decoded_dictionary
 
 
 class BencodingDecodeException(Exception):
-    """Exception raised during decoding of data encoded with Bencoding.
+    """
+    Exception raised during decoding of data encoded with Bencoding.
     """
     pass
 
